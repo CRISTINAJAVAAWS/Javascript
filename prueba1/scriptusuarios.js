f776b2b23c2a1091dc007bfd0238ac5d72d0c601
CRISTINAJAVAAWS <c.barrigaradiologia@gmail.com>
2018-10-25 17:04:48 +0200

    update 25.10

--- prueba1/scriptusuarios.js
+++ prueba1/scriptusuarios.js
@@ -11,8 +11,16 @@ console.log(u1_name+' '+u1_surname);
 console.log(u2_name+' '+u2_surname);
 console.log(u3_name+' '+u3_surname);
 
-function generarNombreCompleto(unNombre,unApellido){
-    return   unNombre+" "+unApellido;
+//Si hago lo siguiente no hay que cambiar la acción, esto es una función y solo necesito modificar, y el resto del codigo lo dejo sin variar.
+function generarNombreCompleto(unNombre, unApellido){
+    let nombre_mod="*"+unNombre+"*";
+    let app_mod="//"+unApellido+"//";
+    return nombre_mod+" "+app_mod;
 }
+//Recomendable crear funciones puras. No acceden a variables globales.
 //Es bueno que tengan un cuerpo y que tengan nombre las funciones
 //Para que devuelva nombre y apellido se pone return y devuelve dos parametros concatenados.
+
+console.log( generarNombreCompleto(u1_name, u1_surname) );
+console.log( generarNombreCompleto(u2_name, u2_surname) );
+console.log( generarNombreCompleto(u3_name, u3_surname) );
