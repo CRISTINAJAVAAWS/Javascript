ec44b1eeec6cc0b1a76cdcd1e31643d1c2f42781
CRISTINAJAVAAWS <c.barrigaradiologia@gmail.com>
2018-10-26 11:53:56 +0200

    Update prueba 2 26.10/script6recursividad.js

---
+++ prueba 2 26.10/script6recursividad.js
@@ -0,0 +1,19 @@
+function una_func(param) {
+    return resultado;
+}
+
+function una_func(param) {
+
+    function factorial(N) {
+        if (N == 0) return 1;
+        else return N * factorial(N-1);
+    }
+
+Console.log(factorial(4));
+
+//Forma arborea, webs, foros...
+
+//Loa algoritmos basados en potencias son mas dificiles de ejecutar. los linealizan y los transforman. No es muy impo.
+//Algoritmos recursivos ocupan menos espacio y codigo. Es mas limpio a la hora de leer y mas complejo de entender.
+//Si quiero encontrar el factorial de un numero. x:multiplicacion de todos los num.3 x 2 x 1
+
