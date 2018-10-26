b68fc0af0655e7795420daaf3f752b75268e3c0f
CRISTINAJAVAAWS <c.barrigaradiologia@gmail.com>
2018-10-26 11:28:38 +0200

    Update prueba 2 26.10/scriptconcatenar.js

--- prueba 2 26.10/scriptconcatenar.js
+++ prueba 2 26.10/scriptconcatenar.js
@@ -1,12 +1,19 @@
-function concatenar(str1,str2){
-return str1+' '+str2;
-}
-console.log( concatenar('Hola','Mundo') );
+
+console.log(concatenar('Hola', 'Mundo'));
 //ES6:
 //Let: Variables
 //const:constantes
+const concatenar= function(str1, str2) {
+    return str1 + ' ' + str2;
+}
+//es una constante
+
+console.log( concatenar2('Hola2', 'Mundo2') );
+
 
 (function(texto){
     console.log(texto);
-
 })('Hola mundo');
+//anonima autoinvocada, ejecuta conforme esta difinda
+
+//funciones maneras de definir; asignando funcion, anonimas autoinvocadas,
