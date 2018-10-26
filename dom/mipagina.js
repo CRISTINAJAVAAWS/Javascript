ebb3b606fc4db03467fcc5d0fffebd3340e644ad
CRISTINAJAVAAWS <c.barrigaradiologia@gmail.com>
2018-10-26 16:55:55 +0200

    update

---
+++ dom/mipagina.js
@@ -0,0 +1,21 @@
+let cambiarboton = document.getElementById('cambiarbtn');
+cambiarboton.onclick = function () {
+    let parrafo = document.getElementById('lorem');
+    parrafo.innerHTML = parrafo.innerHTML.toLowerCase;
+}
+
+let cambiarboton = document.getElementById('cambiarbtn');
+cambiarboton.onclick = function () {
+    let parrafo = document.getElementById('lorem');
+    text.toLowerCase
+
+    let elParr = document.getElementById('parrafo');
+    elParr.innerHTML = 'xxxxxxx';
+}
+
+//cambiarunbotón.
+document.getElementById('upperbtn').onclick-function()¨
+document.onclick
+
+
+
